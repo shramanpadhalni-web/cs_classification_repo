8e74c6159d078bce582d757dcac0ca58a9fd3fd5
hpadhaln <hem.chandra@corestack.io>
2022-02-17 14:39:38 +0530

    These are the model files for image classification

--- cs_classification_repo/features/build_features.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Feature building
-"""
-
-# import project config.py
-import cs_classification_repo.config as cfg
--- cs_classification_repo/visualization/visualize.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Visualization
-"""
-# import project config.py
-import cs_classification_repo.config as cfg
